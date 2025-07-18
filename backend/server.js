e4964414662941abc745f4b402f79ceecbcc3704
Himanshu Yadav <130667230+himanshu-yadv@users.noreply.github.com>
2025-07-18 12:49:09 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
 
 app.get("/", (req, res) => {
-  res.send("API Working")
+  res.send("Server is Working")
 });
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
